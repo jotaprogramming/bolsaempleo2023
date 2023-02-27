be8d446f0c87af68d586d9dc3f0dfb9c7b511fa8
jotaprogramming <yesidlds54@gmail.com>
2023-02-27 10:40:31 -0500

    added configuration module to django admin panel

--- src/config/admin.py
+++ src/config/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+# from .forms import *
+
+
+@admin.register(Countries)
+class CountriesAdmin(admin.ModelAdmin):
+    exclude = ["created_at", "updated_at", "deleted_at"]
+
+
+@admin.register(Districts)
+class DistrictsAdmin(admin.ModelAdmin):
+    exclude = ["created_at", "updated_at", "deleted_at"]
+
+
+@admin.register(Cities)
+class CitiesAdmin(admin.ModelAdmin):
+    exclude = ["created_at", "updated_at", "deleted_at"]
+
+
+@admin.register(Currencies)
+class CurrenciesAdmin(admin.ModelAdmin):
+    exclude = ["created_at", "updated_at", "deleted_at"]
+
+
+@admin.register(DocumentType)
+class DocumentTypeAdmin(admin.ModelAdmin):
+    exclude = []
---
+++ src/config/urls.py
@@ -0,0 +1,12 @@
+# DJANGO MODULES
+from django.contrib import admin
+from django.urls import path
+
+# PROJECT MODULES
+from .views import *
+
+app_name = "config_app"
+
+urlpatterns = [
+    
+]

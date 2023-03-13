7f403199d245a95007931b467f331698e28d1b0f
juniordevMXM <yesidlds54@gmail.com>
2023-03-13 07:52:25 -0500

    static

--- src/static/core/scripts/main.js
+++ src/static/core/scripts/main.js
@@ -33,3 +33,64 @@ editButtons.forEach((element) => {
 deleteButtons.forEach((element) => {
   elementOpenModal(element);
 });
+
+
+
+
+
+
+
+// Slider Assets
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+
+slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+
+function Next(){
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-100%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function (){
+        slider.style.transition ="none";
+        slider.insertAdjacentElement('beforeend',sliderSectionFirst);
+        slider.style.marginLeft = "0";
+
+    }, 500);
+}
+function Prev(){
+    let sliderSection= document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function (){
+        slider.style.transition ="none";
+        slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+
+    }, 500);
+}
+btnRight.addEventListener('click',function(){
+    Next();
+});
+btnLeft.addEventListener('click',function(){
+    Prev();
+});
+
+
+setInterval(() => {
+   Next();
+}, 4000);
+
+
+
+
+
+
+
+
+

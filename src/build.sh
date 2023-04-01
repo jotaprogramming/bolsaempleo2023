b9f34fce26c8f076310153f8a9d322fef86dfee9
juniordevMXM <yesidlds54@gmail.com>
2023-04-01 08:09:19 -0500

    push builder

---
+++ src/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+
+set -o errexit
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput

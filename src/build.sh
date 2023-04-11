4e80963f0914ccfc55c06316398804442c171d00
juniordevMXM <yesidlds54@gmail.com>
2023-04-11 08:51:05 -0500

    makemigrations

--- src/build.sh
+++ src/build.sh
@@ -5,7 +5,8 @@ set -o errexit
 
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+python manage.py makemigrations 
+python manage.py migrate 
 python manage.py collectstatic --no-input
-python manage.py migrate
 
 # DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput

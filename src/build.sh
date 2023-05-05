e7fea5c1f100d6a45ae260f0c1fe95f0ec43aa00
juniordevMXM <yesidlds54@gmail.com>
2023-05-05 17:33:46 -0500

    edit build

--- src/build.sh
+++ src/build.sh
@@ -4,7 +4,7 @@
 set -o errexit
 
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 python manage.py makemigrations 
 python manage.py migrate 
 python manage.py collectstatic --no-input
